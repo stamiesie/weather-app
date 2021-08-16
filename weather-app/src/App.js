a37febaf954cf2b84818194119b3b76f19ef00c8
Stephen Tamiesie <stephen.tamiesie@gmail.com>
2021-08-16 11:55:32 -0700

    fetch current location weather data from API

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,17 +1,28 @@
 import './App.css';
 import React, { useEffect, useState } from 'react';
+// import { fetchData } from './services/apiCall';
 
 export default function App() {
 
   const [lat, setLat] = useState([]);
   const [long, setLong] = useState([]);
+  const [data, setData] = useState([]);
 
   useEffect(() => {
+    const fetchData = async () => {
     navigator.geolocation.getCurrentPosition(function(position) {
       setLat(position.coords.latitude);
       setLong(position.coords.longitude);
     });
 
+      await fetch(`${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`)
+      .then(res => res.json())
+      .then(result => {
+        setData(result)
+        console.log(result);
+      });
+    }
+    fetchData();
     console.log("Latitude is:", lat)
     console.log("Longitude is:", long);
   }, [lat, long]);
